81f0fd2512b124ea5febda27c7aba24e1015d219
RainyArcher <lightrainbowguy@outlook.com>
2022-10-09 13:02:22 +0300

    Added a HttpServer class for making Get requests

---
+++ HttpServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace StorMatch;
+public static class HttpServer
+{
+    public static async Task<string> Get(string url)
+    {
+        try
+        {
+            var client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+            Console.WriteLine(response);
+            return response;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something went wrong", e);
+        }
+        return null;
+    }
+}
--- Views/ConditionsViewModel.cs
+++ Views/ConditionsViewModel.cs
@@ -2,17 +2,22 @@
 using StorMatch.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using StorMatch;
 
 namespace StorMatch.Views;
 public partial class ConditionsViewModel : ObservableObject
 {
+    private const string yandexAPIKey = "e98d1b89-867f-4e6d-9589-26dc06ea9aee";
     public ConditionsViewModel()
     {
-        ConditionsList = new ObservableCollection<WeatherConditions>();
-        ConditionsList.Add(new WeatherConditions{ Name = "Microsoft", Image = "msft.png", TemperatureValue = 12});
-        ConditionsList.Add(new WeatherConditions { Name = "Yandex", Image = "yandex.png", TemperatureValue = 11 });
-        ConditionsList.Add(new WeatherConditions { Name = "Weather.com", Image = "weathercom.png", TemperatureValue = 13 });
+        ConditionsList = new ObservableCollection<WeatherConditions>
+        {
+            new WeatherConditions { Name = "Microsoft", Image = "msft.png", TemperatureValue = 12 },
+            new WeatherConditions { Name = "Yandex", Image = "yandex.png", TemperatureValue = 11 },
+            new WeatherConditions { Name = "Weather.com", Image = "weathercom.png", TemperatureValue = 13 }
+        };
     }
+
     [ObservableProperty]
     ObservableCollection<WeatherConditions> conditionsList;
 
